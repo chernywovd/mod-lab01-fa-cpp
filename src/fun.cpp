1ab15235d58e652417ed65345e4f718b0135ca03
chernywovd <136728354+chernywovd@users.noreply.github.com>
2024-05-12 12:31:03 +0300

    Чернышов И. 382007-3

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,7 +2,30 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    int NumberOfWordsWithoutDigits = 0;
+    bool ContainsDigit = false;
+    bool ContainsLetter = false;
+    
+    while (*str != '\0') {// Чернышов И. идем по строка пока не дойдем до ее конца 
+        if (std::isalpha(static_cast<unsigned char>(*str))) { // Функция isalpha проверяет аргумент, передаваемый через параметр сharacter, является ли он строчной или прописной буквой алфавита
+            if (!ContainsLetter) {
+                ContainsLetter = true;
+            }
+        } else if (std::isdigit(static_cast<unsigned char>(*str))) {
+            ContainsDigit = true;
+        } else {
+            if (ContainsLetter && !ContainsDigit) {
+                NumberOfWordsWithoutDigits++;
+            }
+            ContainsLetter = false;
+            ContainsDigit = false;
+        }
+        str++;
+    }
+    if (ContainsLetter && !ContainsDigit) {
+        NumberOfWordsWithoutDigits++;
+    }
+    return NumberOfWordsWithoutDigits;
 }
 
 unsigned int faStr2(const char *str) {
